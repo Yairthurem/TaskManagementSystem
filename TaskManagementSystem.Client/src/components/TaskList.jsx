14266a2a74b2c40512ae23b4853e820d7f912796
yairt <yairthurem@gmail.com>
2026-04-04 17:25:25 +0300

    Reduce polling time spans

--- TaskManagementSystem.Client/src/components/TaskList.jsx
+++ TaskManagementSystem.Client/src/components/TaskList.jsx
@@ -6,7 +6,7 @@ export default function TaskList() {
   const navigate = useNavigate()
   const [selectedUserId, setSelectedUserId] = useState('')
   const { data: tasks, isLoading, isError } = useGetTasksQuery(undefined, { 
-    pollingInterval: 30000 // Refresh every 30s to catch background updates
+    pollingInterval: 20000 // Refresh every 20s to catch background updates
   })
   const { data: users } = useGetUsersQuery()
   const [deleteTask] = useDeleteTaskMutation()
