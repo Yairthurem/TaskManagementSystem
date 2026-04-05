42491128b02bcb61a9ba0797dc555c697a07d6e1
yairt <yairthurem@gmail.com>
2026-04-05 17:46:01 +0300

    perf: enable global split query behavior for efficient collection loading

--- TaskManagementSystem.Api/Program.cs
+++ TaskManagementSystem.Api/Program.cs
@@ -40,7 +40,8 @@ builder.Services.AddSwaggerGen();
 
 // Register DbContext
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
+        sqlOptions => sqlOptions.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery)));
 
 // Dependency Injection for Repositories and Services
 builder.Services.AddScoped<ITaskRepository, TaskRepository>();
